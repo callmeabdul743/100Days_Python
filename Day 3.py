afeb87236beb902b5fd9ceec96ed715f596695b4
callmeabdul743 <abdullahkhalidmul@gmail.com>
2026-06-20 22:15:39 +0500

    Day 3 is if else

--- Day 3.py
+++ Day 3.py
@@ -1,5 +1,21 @@
-even = int(input("Enter a number: "))
-if even % 2 ==0:
-    print("The number is Even.")
+# even = int(input("Enter a number: "))
+# if even % 2 ==0:
+#     print("The number is Even.")
+# else:
+#     print("The number is odd.")
+
+
+# weight = 85
+# height = 1.85
+
+# bmi = weight / (height ** 2)
+bmi = 23
+if bmi < 18.5:
+    print("underweight")
+elif bmi >= 18.5 and bmi <25:
+    print("normal weight")
 else:
-    print("The number is odd.")
+    print("overweight")
+
+# 🚨 Do not modify the values above
+# Write your code below 👇
