01394555b0df1a0e283e726cce78838f3683457b
callmeabdul743 <abdullahkhalidmul@gmail.com>
2026-06-22 20:37:49 +0500

    range in python

--- Day 5.py
+++ Day 5.py
@@ -1,6 +1,11 @@
-arr = [1,2,45, 66,67,77, 99, 100, 123]
-maxnum = arr[0]
-for aa in arr:
-    if maxnum < aa:
-        maxnum = aa
-print(maxnum)
+# arr = [1,2,45, 66,67,77, 99, 100, 123]
+# maxnum = arr[0]
+# for aa in arr:
+#     if maxnum < aa:
+#         maxnum = aa
+# print(maxnum)
+# range in python
+# a = 0
+# for b in range(1,101):
+#     a += b
+# print(a)
