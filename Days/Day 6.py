7b2b9da26d8b68d27835534558b123297c864981
callmeabdul743 <abdullahkhalidmul@gmail.com>
2026-06-24 14:59:36 +0500

    This is the maze code of the pyton

---
+++ Days/Day 6.py
@@ -0,0 +1,25 @@
+def mov():
+    move()
+    move()
+    move()
+def right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def l():
+    mov()
+    turn_left()
+    mov()
+    right()
+
+def to_ro():
+    move()
+    right()
+l()
+to_ro()
+l()
+to_ro()
+l()
+to_ro()
+l()
