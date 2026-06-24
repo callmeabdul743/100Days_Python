cbd43959ebcc946e7695677ac9b9bd51dab15a94
callmeabdul743 <abdullahkhalidmul@gmail.com>
2026-06-24 07:13:53 +0500

    Started the day 6

---
+++ Days/Day 5.py
@@ -0,0 +1,33 @@
+# arr = [1,2,45, 66,67,77, 99, 100, 123]
+# maxnum = arr[0]
+# for aa in arr:
+#     if maxnum < aa:
+#         maxnum = aa
+# print(maxnum)
+
+
+# range in python
+# a = 0
+# for b in range(1,101):
+#     a += b
+# print(a)
+
+
+# for a in range (1, 101):
+#     if a%3 == 0 and a%5== 0:
+#         print("FizzBuzz")
+#     elif a%3 == 0:
+#         print("Fizz")
+#     elif a%5 == 0:
+#         print("Buzz")
+#     else:
+#         print(a)
+
+import random
+
+a = [1, 2,3, 4, 5,6 ]
+print(a)
+random.shuffle(a)
+print(a)
+random.shuffle(a)
+print(a)
